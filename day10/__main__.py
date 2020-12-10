18362be2399d36ebbc1d419e00fe62b59f0e76ca
Sander van Kasteel <22967311+svankasteel@users.noreply.github.com>
2020-12-10 09:16:51 +0100

    add solution to day 10

--- day10/__main__.py
+++ day10/__main__.py
@@ -2,7 +2,57 @@ import sys
 
 
 def main(args=None):
-    pass
+    file = open('day10/input.txt', 'r')
+
+    lines = file.read().splitlines()
+
+    joltages = list(map(int, lines))
+
+    outlet = 0
+    device = max(joltages) + 3
+
+    joltages.append(outlet)
+    joltages.append(device)
+    
+    answer_1 = calc_diff_of_adapters(joltages)
+    
+    print(answer_1)
+
+    answer_2 = calc_arrangements(joltages)
+
+    print(answer_2)
+
+def calc_diff_of_adapters(joltages: [int]) -> int:
+    js = sorted(joltages)
+    ones = 0
+    threes = 0
+
+    for i in range(len(js)-1):
+        diff = js[i+1] - js[i]
+        if diff == 1:
+            ones += 1
+        if diff == 3:
+            threes +=1
+    
+    return ones * threes
+
+store = {}
+
+def calc_arrangements(joltages: [int]) -> int:
+    for j in sorted(joltages, reverse=True):
+        connections = get_connections(j, joltages)
+
+        if connections == []:
+            store[j] = 1
+        else:
+            print([ store[c] for c in connections ])
+            store[j] = sum([ store[c] for c in connections ])
+    
+    return store[0]
+
+def get_connections(joltage: int, all: [int]) -> [int]:
+    candidates = { joltage+1, joltage+2, joltage+3 }
+    return list(candidates.intersection(set(all)))
 
 if __name__ == "__main__":
     sys.exit(main())
